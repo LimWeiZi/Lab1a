4c62f48dad290619d9548589c48339efcf0ff2f7
dtrine222 <33109121+dtrine222@users.noreply.github.com>
2017-10-26 11:26:37 +0800

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Hello World Script</title></head>
+<body>
+<h1>
+this is heading 1
+</h1>
+<p> Hello World! in HTML </p>
+
+<?php                                                                        
+	echo "<p>Hello World! in PHP </p>";
+?>                                             
+
+</body>
+</html>
